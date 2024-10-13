7fd889d0d2b7d2b4ddd1edff9e1dd337868a57cc
lena-goldchen <87316843+lena-goldchen@users.noreply.github.com>
2024-10-13 23:09:03 +0300

    Reload portfolio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger'),
+  menu = document.querySelector('.menu'),
+  closeElem = document.querySelector('.menu__close');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');
 })
+
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills-levels__counter'),
+  lines = document.querySelectorAll('.skills-levels__line span');
+
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
